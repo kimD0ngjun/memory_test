90bbec3261737a6ab5a01f2f5dd8fcf602286e08
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-24 03:06:55 +0900

    [feat] 버튼 전부 깜빡임 함수 작성

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 let memoryButton = document.getElementsByClassName("memoryButton");
 const memoryArray = Array.from(memoryButton); // 유사배열객체 배열화
+
+console.log(memoryArray);
+
+let playButton = document.getElementById("playButton");
+
+let stageSequence = [];
+let index = 0;
+let stage = 1;
+
+playButton.addEventListener("click", game);
+
+function game() {
+  allBlink();
+}
+
+function allBlink() {
+  return new Promise((resolve) => {
+    console.log("반짝");
+
+    memoryArray.map((el) => el.classList.add(`message`));
+    setTimeout(() => {
+      memoryArray.map((el) => el.classList.remove(`message`));
+      resolve();
+    }, 500);
+  });
+}
