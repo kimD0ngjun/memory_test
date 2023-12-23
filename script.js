2535de4ad0076089c81f981b824da69cd37b9d46
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-24 03:10:25 +0900

    [feat] 성공 메시지 버튼 깜빡임 함수 작성

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ const memoryArray = Array.from(memoryButton); // 유사배열객체 배열화
 console.log(memoryArray);
 
 let playButton = document.getElementById("playButton");
-
-let stageSequence = [];
-let index = 0;
 let stage = 1;
 
 playButton.addEventListener("click", game);
 
-function game() {
-  allBlink();
+async function game() {
+  await allBlink();
+  await successBlink();
 }
 
 function allBlink() {
@@ -22,6 +20,27 @@ function allBlink() {
     memoryArray.map((el) => el.classList.add(`message`));
     setTimeout(() => {
       memoryArray.map((el) => el.classList.remove(`message`));
+      resolve();
+    }, 500);
+  });
+}
+
+function successBlink() {
+  return new Promise((resolve) => {
+    console.log("성공 반짝");
+
+    memoryArray.map((el) =>
+      (Number(el.id) > 1 && Number(el.id) < 5) ||
+      (Number(el.id) > 21 && Number(el.id) < 25) ||
+      (Number(el.id) % 5 === 1 &&
+        Number(el.id) !== 1 &&
+        Number(el.id) !== 21) ||
+      (Number(el.id) % 5 === 0 && Number(el.id) !== 5 && Number(el.id) !== 25)
+        ? el.classList.add(`success`)
+        : el
+    );
+    setTimeout(() => {
+      memoryArray.map((el) => el.classList.remove(`success`));
       resolve();
     }, 500);
   });
